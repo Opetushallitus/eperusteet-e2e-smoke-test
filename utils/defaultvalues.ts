01f47a7d9513d272610dfb58e56f197abea74ec4
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-04-05 12:12:28 +0300

    playwright

---
+++ utils/defaultvalues.ts
@@ -0,0 +1,19 @@
+export const CI_baseUrl = 'https://virkailija.hahtuvaopintopolku.fi';
+
+
+export const CI_VALUES = {
+  baseUrl: 'https://virkailija.hahtuvaopintopolku.fi',
+  eperusteUrl: 'https://virkailija.hahtuvaopintopolku.fi/eperusteet-app',
+}
+
+export const LOCAL_VALUES = {
+  baseUrl: 'http://localhost:9001',
+  eperusteUrl: 'http://localhost:9001',
+}
+
+export const ENVIRONMENT_VALUES = process.env.CI ? CI_VALUES : LOCAL_VALUES;
+
+export const DEFAULT_VALUES = {
+  loginUrl: `${ENVIRONMENT_VALUES.baseUrl}/`,
+  uusiPerusteUrl: `${ENVIRONMENT_VALUES.eperusteUrl}/#/fi/perusteprojektit/uusi`,
+}
