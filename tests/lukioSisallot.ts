ed4b8464d3ac34b2289beba2ece013f6e2d8c1ce
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2025-01-30 17:17:33 +0200

    lukio-tests

---
+++ tests/lukioSisallot.ts
@@ -0,0 +1,167 @@
+import { expect, Page } from "@playwright/test";
+import { waitMedium } from "../utils/commonmethods";
+import { DEFAULT_VALUES } from "../utils/defaultvalues";
+
+const oppiaineet = [
+  {nimi: 'Historia', moduuliNimi: 'Antiikin elämää', oppimaaraNimi: 'Biologia'},
+  {nimi: 'Fysiikka', moduuliNimi: 'Analyysi ja jatkuva jakauma', oppimaaraNimi: 'Elämänkatsomustieto'},
+  {nimi: 'Filosofia', moduuliNimi: 'Aktiivinen elämä', oppimaaraNimi: 'Juutalainen uskonto'}
+];
+
+const laot = [
+  {nimi: 'Hyvinvointiosaaminen'},
+  {nimi: 'Yhteiskunnallinen osaaminen'},
+];
+
+export async function lukioSisallot(page: Page, url: string) {
+
+  for (const oppiaine of oppiaineet) {
+    await page.goto(url + 'lukio/oppiaineet');
+    await lisaaOppiaine(page, oppiaine.nimi, oppiaine.moduuliNimi, oppiaine.oppimaaraNimi);
+  }
+
+  for(const lao of laot) {
+    await page.goto(url + 'lukio/laajaalaisetosaamiset');
+    await lisaaLaajaAlainenOsaaminen(page, lao.nimi, laot.indexOf(lao));
+  }
+}
+
+async function lisaaLaajaAlainenOsaaminen(page: Page, nimi: string, index: number) {
+  await page.getByRole('button', { name: 'Muokkaa' }).click();
+  await page.getByRole('button', { name: 'Uusi laaja-alainen' }).click();
+  await page.getByRole('button', { name: 'Hae koodistosta' }).nth(index).click();
+  await expect(page.locator('.modal-content')).toContainText('Hyvinvointiosaaminen');
+  await page.locator('.modal-content').getByText(nimi).click();
+  await page.locator('.ProseMirror').nth(index).fill(nimi + ' kuvaus');
+
+  await page.getByRole('button', { name: 'Tallenna' }).click();
+  await expect(page.locator('.notification')).toContainText('Tallennus onnistui');
+  await expect(page.locator('.editointi-container')).toContainText('Muokkaa');
+}
+
+async function lisaaOppiaine(page: Page, nimi: string, moduuliNimi: string, oppimaaraNimi: string) {
+  await page.getByRole('button', { name: 'Uusi oppiaine' }).click();
+  await page.getByRole('button', { name: 'Hae koodistosta' }).click();
+  await expect(page.locator('.modal-content')).toContainText(nimi);
+  await page.locator('.modal-content').getByText(nimi).click();
+  await page.locator('.ProseMirror').nth(0).fill(nimi + ' tehtava');
+  await page.locator('.ProseMirror').nth(1).fill(nimi + ' lao');
+  await page.locator('.ProseMirror').nth(2).fill(nimi + ' tavoite');
+  await page.locator('.ProseMirror').nth(3).fill(nimi + ' osaamisen arviointi');
+  await page.locator('.ProseMirror').nth(4).fill(nimi + ' pakollinen moduuli');
+  await page.locator('.ProseMirror').nth(5).fill(nimi + ' valinnainen moduuli');
+  await page.getByRole('button', { name: 'Lisää tavoitealue', exact: true }).click();
+  await page.getByRole('button', { name: 'Lisää tavoite', exact: true }).click();
+  await page.locator('.tavoitealue').nth(0).getByRole('textbox').nth(0).fill(nimi + ' tavoitealue');
+  await page.locator('.tavoitealue').nth(0).getByRole('textbox').nth(1).fill(nimi + ' kohde');
+  await page.locator('.tavoitealue').nth(0).getByRole('textbox').nth(2).fill(nimi + ' tavoite');
+
+  await page.getByRole('button', { name: 'Tallenna' }).click();
+  await expect(page.locator('.notification')).toContainText('Tallennus onnistui');
+  await expect(page.locator('.editointi-container')).toContainText('Muokkaa');
+
+  await page.getByRole('button', { name: 'Uusi oppimäärä' }).click();
+  await page.getByRole('button', { name: 'Hae koodistosta' }).click();
+  await expect(page.locator('.modal-content')).toContainText(oppimaaraNimi);
+  await page.locator('.modal-content').getByText(oppimaaraNimi).click();
+
+  await page.getByRole('button', { name: 'Tallenna' }).click();
+  await expect(page.locator('.notification')).toContainText('Tallennus onnistui');
+  await expect(page.locator('.editointi-container')).toContainText('Muokkaa');
+
+  await page.getByRole('button', { name: 'Lisää pakollinen moduuli' }).click();
+  await page.getByRole('button', { name: 'Hae koodistosta' }).click();
+  await expect(page.locator('.modal-content')).toContainText(moduuliNimi);
+  await page.locator('.modal-content').getByText(moduuliNimi).click();
+  await page.getByText('Pakollinen').check();
+  await page.getByRole('spinbutton').nth(0).fill('5');
+  await page.locator('.ProseMirror').nth(0).fill(moduuliNimi + ' kuvaus');
+
+  await page.getByRole('button', { name: 'Tallenna' }).click();
+  await expect(page.locator('.notification')).toContainText('Tallennus onnistui');
+  await expect(page.locator('.editointi-container')).toContainText('Muokkaa');
+}
+
+export async function lukioJulkinenTarkistukset(page: Page) {
+  await page.locator('.navigation-tree').getByText('Laaja-alaisen osaamisen osa-alueet').click();
+  for (const lao of laot) {
+    await expect(page.locator('.navigation-tree')).toContainText(lao.nimi);
+    await expect(page.locator('.laaja-alaiset')).toContainText(lao.nimi + ' kuvaus');
+  };
+
+  await page.locator('.navigation-tree').getByText('Oppiaineet').click();
+  for (const oppiaine of oppiaineet) {
+    await page.locator('.navigation-tree').getByText(oppiaine.nimi).click();
+    await expect(page.locator('.navigation-tree')).toContainText(oppiaine.nimi);
+    await expect(page.locator('.navigation-tree')).toContainText(oppiaine.oppimaaraNimi);
+    await expect(page.locator('.content')).toContainText(oppiaine.nimi + ' tehtava');
+    await expect(page.locator('.content')).toContainText(oppiaine.nimi + ' lao');
+    await expect(page.locator('.content')).toContainText(oppiaine.nimi + ' tavoite');
+    await expect(page.locator('.content')).toContainText(oppiaine.nimi + ' tavoitealue');
+    await expect(page.locator('.content')).toContainText(oppiaine.nimi + ' osaamisen arviointi');
+
+    await page.locator('.navigation-tree').getByText(oppiaine.oppimaaraNimi).click();
+    await expect(page.locator('.navigation-tree')).toContainText(oppiaine.moduuliNimi);
+    await expect(page.locator('.content')).toContainText(oppiaine.moduuliNimi);
+    await expect(page.locator('.content')).toContainText('5 op');
+    await page.locator('.content').getByRole('button', { name: oppiaine.moduuliNimi } ).click();
+    await expect(page.locator('.content')).toContainText("Pakollinen");
+    await expect(page.locator('.content')).toContainText("5 op");
+  }
+}
+
+export async function lukioOpsLuonti(page: Page) {
+  await page.getByLabel("Ei", { exact: true }).nth(0).click({ force: true});
+  await page.getByLabel("Ei", { exact: true }).nth(1).click({ force: true});
+}
+
+export async function lukioOpsSisallot(page: Page) {
+  for (const oppiaine of oppiaineet) {
+    await page.getByRole('link', { name: 'Yleisnäkymä' }).click();
+    await page.getByRole('link', { name: 'Oppiaineet' }).click();
+    await page.locator('.navigation').getByRole('link', { name: oppiaine.nimi }).click();
+    await expect(page.locator('.navigation')).toContainText(oppiaine.nimi);
+    await expect(page.locator('.navigation')).toContainText(oppiaine.oppimaaraNimi);
+    await expect(page.locator('.content')).toContainText(oppiaine.nimi + ' tehtava');
+    await expect(page.locator('.content')).toContainText(oppiaine.nimi + ' lao');
+    await expect(page.locator('.content')).toContainText(oppiaine.nimi + ' tavoite');
+    await expect(page.locator('.content')).toContainText(oppiaine.nimi + ' tavoitealue');
+    await expect(page.locator('.content')).toContainText(oppiaine.nimi + ' osaamisen arviointi');
+    await expect(page.locator('.content')).toContainText(oppiaine.oppimaaraNimi);
+
+    await page.locator('.navigation').getByRole('link', { name: oppiaine.oppimaaraNimi }).click();
+    await page.locator('.content').getByRole('button', { name: 'Uusi opintojakso' }).click();
+    await expect(page.locator('.editointikontrolli')).toContainText('Peruuta');
+    await expect(page.locator('.editointikontrolli')).toContainText('Tallenna');
+    await page.locator('.content').getByRole('textbox').nth(0).fill(oppiaine.oppimaaraNimi + ' opintojakso');
+    await page.locator('.content').getByRole('textbox').nth(1).fill('opintojakso_' + oppiaineet.indexOf(oppiaine));
+    await page.locator('.moduuli').getByRole('button', { name: oppiaine.moduuliNimi }).click();
+
+    await page.mouse.wheel(0, -1080);
+    await page.getByRole('button', { name: 'Tallenna' }).click();
+    await expect(page.locator('.notification')).toContainText('Opintojakson luominen onnistui');
+    await expect(page.locator('.editointikontrolli')).toContainText('Muokkaa');
+  }
+}
+
+export async function lukioJulkinenOpsTarkistukset(page: Page) {
+  await page.locator('.navigation-tree').getByText('Oppiaineet').click();
+  for (const oppiaine of oppiaineet) {
+    await page.locator('.navigation-tree').getByText(oppiaine.nimi).click();
+    await expect(page.locator('.navigation-tree')).toContainText(oppiaine.oppimaaraNimi);
+    await expect(page.locator('.content')).toContainText(oppiaine.nimi + ' tehtava');
+    await expect(page.locator('.content')).toContainText(oppiaine.nimi + ' lao');
+    await expect(page.locator('.content')).toContainText(oppiaine.nimi + ' tavoite');
+    await expect(page.locator('.content')).toContainText(oppiaine.nimi + ' tavoitealue');
+    await expect(page.locator('.content')).toContainText(oppiaine.nimi + ' osaamisen arviointi');
+    await expect(page.locator('.content')).toContainText(oppiaine.oppimaaraNimi);
+    await page.locator('.content').getByText(oppiaine.oppimaaraNimi).nth(0).click();
+
+    await expect(page.locator('.navigation-tree')).toContainText(oppiaine.oppimaaraNimi + ' opintojakso');
+    await expect(page.locator('.navigation-tree')).toContainText(oppiaine.moduuliNimi);
+
+    await page.locator('.navigation-tree').getByText(oppiaine.moduuliNimi).click();
+    await expect(page.locator('.content')).toContainText("Pakollinen");
+    await expect(page.locator('.content')).toContainText("5 op");
+  }
+}
--- utils/commonmethods.ts
+++ utils/commonmethods.ts
@@ -3,9 +3,11 @@ import { Page } from '@playwright/test';
 export const login = async (page: Page, url) => {
   if (process.env.CI) {
     await page.goto(url);
-    await page.locator('#username').fill(process.env.TEST_AUTOMATION_USERNAME!);
-    await page.locator('#password').fill(process.env.TEST_AUTOMATION_PASSWORD!);
-    await page.locator('input[type="submit"]').click();
+    if (await page.locator('#username').isVisible()) {
+      await page.locator('#username').fill(process.env.TEST_AUTOMATION_USERNAME!);
+      await page.locator('#password').fill(process.env.TEST_AUTOMATION_PASSWORD!);
+      await page.locator('input[type="submit"]').click();
+    }
   } else {
     await page.goto(url);
   }
