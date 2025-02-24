409b457427b510d91f7c3581dcbdaec90db7ae9b
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2025-02-24 13:04:28 +0200

    test-fixes

--- utils/commonmethods.ts
+++ utils/commonmethods.ts
@@ -3,11 +3,9 @@ import { Page } from '@playwright/test';
 export const login = async (page: Page, url) => {
   if (process.env.CI) {
     await page.goto(url);
-    if (await page.locator('#username').isVisible()) {
-      await page.locator('#username').fill(process.env.TEST_AUTOMATION_USERNAME!);
-      await page.locator('#password').fill(process.env.TEST_AUTOMATION_PASSWORD!);
-      await page.locator('input[type="submit"]').click();
-    }
+    await page.locator('#username').fill(process.env.TEST_AUTOMATION_USERNAME!);
+    await page.locator('#password').fill(process.env.TEST_AUTOMATION_PASSWORD!);
+    await page.locator('input[type="submit"]').click();
   } else {
     await page.goto(url);
   }
